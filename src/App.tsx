94f0395e5d56b4293a4321476802fa452fd2fd09
webala <webd01@protonmail.com>
2024-03-02 15:52:55 +0300

    starter template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import React from "react";
-
+import { useState } from "react";
+import "./App.css";
 const App = () => {
-  return <div></div>;
+
+  const [counterValue, setCounterValue] = useState(0)
+
+  const availableHouses = [
+    "Malubu",
+    "Purple royal",
+    "Golden Rush",
+    "White innocence",
+  ];
+  const [bookedHouses, setHookedHouses] = useState([]);
+  return (
+    <div className="main">
+      <div>
+        <h1>Simple counter</h1>
+        <div className="counter-actions">
+          <button>Add</button>
+          <p>{counterValue}</p>
+          <button>Subtract</button>
+        </div>
+      </div>
+      <div>
+        <h1>Available houses</h1>
+        {availableHouses.map((house) => (
+          <div key={crypto.randomUUID()} className="house">
+            <p>{house}</p> <button>Add to booked houses</button>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h1>Booked houses</h1>
+        {bookedHouses.map((house) => (
+          <div key={crypto.randomUUID()} className="house">
+            <p>{house}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
